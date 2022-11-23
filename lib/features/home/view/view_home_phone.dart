f024dc46c1abd6d41300d14f4a9ca2021d1d2eb1
Mar Bert John Mataverde <Marbertmataverde.dev@gmail.com>
2022-11-23 14:55:05 +0800

    update: home phone initial commit

---
+++ lib/features/home/view/view_home_phone.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+import 'package:pezo/features/home/widgets/bottom%20navbar/bottom_nav_bar.dart';
+import 'package:pezo/features/home/widgets/drawer/drawer.dart';
+
+class HomePhone extends StatelessWidget {
+  const HomePhone({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        elevation: 0,
+        backgroundColor: Colors.transparent,
+      ),
+      drawer: buildDrawer(),
+      body: const Center(
+        child: Text('Page Name'),
+      ),
+      bottomNavigationBar: buildBottomNavigationBar(),
+    );
+  }
+}
